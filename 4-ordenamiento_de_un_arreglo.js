1280d0a4e4419b47d7fe53f52df8ec3bf6c5444f
Armando Jair Ibañez Parra <jairibaparr@outlook.com>
2024-08-28 18:37:36 -0700

    Create 4-ordenamiento_de_un_arreglo.js

---
+++ 4-ordenamiento_de_un_arreglo.js
@@ -0,0 +1,12 @@
+// Ordenamiento de un arreglo:
+// Crea un arreglo de números desordenados y luego ordénalo de menor a mayor utilizando algún algoritmo de ordenamiento
+// (p. ej., el método sort()).
+
+// Arreglo desordenado
+const numeros = [34, 7, 23, 32, 5, 62, 13];
+
+// Ordenamiento de un arreglo
+numeros.sort((a, b) => a - b);
+
+// Mostrar arreglo ordenado con metodo sort
+console.log("Arreglo ordenado:", numeros);
